26b4e279fffe715a78404228b0d04e94a076894f
juanalbertomontoya <67620654+juanalbertomontoya@users.noreply.github.com>
2020-09-05 20:29:25 -0500

    screen, burbuja y menu
    creamos el full screen de la columna uno, añadimos iconos a la burbuja emergente y colocamos la disposicion del menu con la funcion de cada boton

--- galeria-fullscreen.js
+++ galeria-fullscreen.js
@@ -1,6 +1,68 @@
-//abre el background del fullscreen al hacer click en un cuadro de la galeria
+// funcion que le da clase clicked a cuadro
+
+$(document).ready(function () {
+
+  //this will attach the class to every target 
+  $(document).on('click', function (event) {
+      $target = $(event.target);   
+         $target.addClass('clicked');
+     });
+
+ });
+
+
+
+
+//funcion para aparecer el fullscreen
 
 $('.cuadro-galeria').on('click',function() {
+    
+    
+    //coloca el clicked en el cuadro seleccionado
+
+    $('').addClass('clicked');
+
+    //muestra la imagen en fullscreen dependiendo de a que cuadro se haya hecho click
+
+    setTimeout(function () {
+
+      //muestra la primera imagen
+      
+      if ($('#galeria-imagen1').hasClass("clicked")) {
+
+        $('.contenedor-imagenfull').css('margin-left', '-'+100+'%');
+  
+    }
+
+     //muestra la segunda imagen
+  
+     else if ($('#galeria-imagen2').hasClass("clicked")) {
+  
+      $('.contenedor-imagenfull').css('margin-left', '-'+200+'%');
+  
+     }
+
+     //muestra la tercera imagen
+  
+    else if ($('#galeria-imagen3').hasClass("clicked")) {
+  
+      $('.contenedor-imagenfull').css('margin-left', '-'+300+'%');
+  
+    }
+
+     //muestra la cuarta imagen
+  
+     else {
+  
+      $('.contenedor-imagenfull').css('margin-left', '-'+400+'%');
+  
+    }
+
+    }, 500);
+
+
+    //abre el background del fullscreen al hacer click en un cuadro de la galeria
+
     $('.galeria-fullscreen').removeClass('off');
     $('.galeria-fullscreen').addClass('on');
 
@@ -22,8 +84,94 @@ $('.cuadro-galeria').on('click',function() {
       //boton de salir
 
       $('.exit').addClass('on');
-      }, 2700)
+      }, 2700);
+
+
  });
+
+
+
+ //funciones para desplazar el fullscreen con botones
+
+
+
+
+function moverFull () {
+
+  //almacenar slider en una variable//
+var sliderFull = $('.contenedor-imagenfull');
+//almacenar botones//
+var siguienteFull = $('#btn-next-full');
+var anteriorFull = $('#btn-prev-full');
+
+
+//mover ultima columna al primer lugar//
+
+ $('.contenedor-imagenfull div:last').insertBefore('.contenedor-imagenfull div:first');
+
+ sliderFull.css('margin-left', '-'+100+'%');
+
+
+ // mover a la derecha
+
+ function moverD() {
+
+
+  // mueve el slider a la derecha
+  sliderFull.animate({marginLeft:'-'+200+'%'
+  } ,900, 
+  function(){
+
+    $('.contenedor-imagenfull div:first').insertAfter('.contenedor-imagenfull div:last');
+    sliderFull.css('margin-left', '-'+100+'%');
+
+    
+  }
+  );
+     
+ }
+
+ siguienteFull.on('click',function() {
+  moverD();
+ });
+
+
+
+ // mover a la izquierda
+
+ function moverI() {
+
+
+
+  //mueve el slider a la izquierda
+
+  sliderFull.animate({marginLeft: 0
+  } ,900, 
+  function(){
+    $('.contenedor-imagenfull div:last').insertBefore('.contenedor-imagenfull div:first');
+    sliderFull.css('margin-left', '-'+100+'%');
+  }
+  );
+
+
+  
+ }
+
+ anteriorFull.on('click',function() {
+  moverI();
+ });
+
+ 
+}
+
+ //llamada a la funcionb que mueve botones
+
+ moverFull();
+
+
+
+
+
 
  //cierra el background 
 
@@ -42,9 +190,34 @@ $('.cuadro-galeria').on('click',function() {
 
   $('.exit').removeClass('on');
 
+  //elimina la clase clicked en todos los elementos
+
+  setTimeout(function () {
+    $('.cuadro-galeria').removeClass('clicked');
+  },700);
+
 });
 
 
 //hace zoom en la imagen al hacer hover
 
-$('.imagenfull').zoom();
+$(document).ready(function(){
+  $('.galeria-fullscreen img')
+    .wrap('<span style="display:inline-block"></span>')
+    .css('display', 'block')
+    .parent()
+    .zoom();
+});
+
+
+
+
+
+
+
+//--------------------------------------------------------//
+//configuraciones del cambio de imagenes en el full screen//
+//-------------------------------------------------------//
+
+
+
---
+++ reduce-menu.js
@@ -0,0 +1,17 @@
+
+
+$(window).scroll(function () {
+  var scroll = $(window).scrollTop();
+
+  if (scroll > 2) {
+    $('.logo-menu a').addClass('scroll');
+    $('.logo-menu img').addClass('scroll');
+    $('.menu').addClass('scroll');
+  }
+
+  else {
+    $('.logo-menu a').removeClass('scroll');
+    $('.logo-menu img').removeClass('scroll');
+    $('.menu').removeClass('scroll');
+  }
+});
